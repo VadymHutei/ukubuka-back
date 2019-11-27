8d380420f53d9716f12968160fbcd48f5163a083
Vadym Hutei <hutei@live.com>
2019-11-27 08:33:15 +0200

    [RESTful]

--- app/main.py
+++ app/main.py
@@ -1,31 +1,55 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_restful import Resource, Api
+
 
 app = Flask(__name__)
+api = Api(app)
+
+class Menus(Resource):
+    '''
+
+    ARGUMENT    DEFAULT VALUE   ALTERNATIVE VALUE
+    structure   list            tree
+    active      Y               N
+    parent      None            <item_id>
+
+    '''
+    def get(self):
+        return jsonify(
+            [
+                {
+                    'id': 2,
+                    'name': 'Квіти',
+                    'parent': 1,
+                    'link': '/flowers/',
+                    'order': 100
+                },
+                {
+                    'id': 4,
+                    'name': 'Букети',
+                    'parent': 2,
+                    'link': '/flowers/bouquets/',
+                    'order': 10
+                },
+                {
+                    'id': 5,
+                    'name': 'Рослини',
+                    'parent': 2,
+                    'link': '/flowers/plants/',
+                    'order': 20
+                },
+                {
+                    'id': 3,
+                    'name': 'Меблі',
+                    'parent': 1,
+                    'link': '/furniture/',
+                    'order': 100
+                }
+            ]
+        )
+
+api.add_resource(Menus, '/menu')
 
 @app.route('/', methods=['GET'])
 def main():
     return 'Back works!'
-
-@app.route('/categories/', methods=['GET'])
-def categories():
-    return 'Categories works!'
-
-@app.route('/products/', methods=['GET'])
-def products():
-    return 'Products works!'
-
-@app.route('/sku/', methods=['GET'])
-def sku():
-    return 'Sku works!'
-
-@app.route('/characteristics/', methods=['GET'])
-def characteristics():
-    return 'Characteristics works!'
-
-@app.route('/users/', methods=['GET'])
-def users():
-    return 'Users works!'
-
-@app.route('/menus/', methods=['GET'])
-def menus():
-    return 'Menus works!'
