a1adddc1a8de27f0d67345527be204a3a24232e9
Vadym Hutei <hutei@live.com>
2019-12-11 20:19:02 +0200

    [MENU] added Menu repository

---
+++ app/repositories/__init__.py
@@ -0,0 +1,1 @@
+from repositories.menu import MenuRepo
---
+++ app/repositories/menu.py
@@ -0,0 +1,10 @@
+from core import Repository
+
+
+class MenuRepo(Repository):
+
+    def getMenuByID(self, id_) -> list:
+        return []
+
+    def getMenuByAlias(self, alias) -> list:
+        return []
