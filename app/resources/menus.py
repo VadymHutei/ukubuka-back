fe1cb2fe71801f36a2b638de3d060473fe6c8ad2
Vadym Hutei <hutei@live.com>
2019-12-12 21:30:08 +0200

    [MENU] resource

--- app/resources/menus.py
+++ app/resources/menus.py
@@ -1,38 +1,59 @@
 from flask import jsonify
-from flask_restful import Resource, reqparse, abort
+from flask_restful import reqparse, abort
 
-from core import validator
+from core import Resource, validator
 from repositories import MenuRepo
 
 
 class Menus(Resource):
 
-    def get(self):
+    def _setArguments(self) -> None:
         parser = reqparse.RequestParser()
-        parser.add_argument('parent', type=int, location='args')
+        parser.add_argument('id', type=int, location='args')
         parser.add_argument('alias', location='args')
         parser.add_argument('active', location='args')
         parser.add_argument('order', location='args')
         parser.add_argument('language', location='headers')
-        args = parser.parse_args()
+        self._args = parser.parse_args()
 
-        param = {}
-        if args['active'] is not None and validator.active(args['active']):
-            param['active'] = args['active'].upper()
-        if args['order'] is not None and validator.orderDirection(args['order']):
-            param['order'] = args['order'].upper()
-        if args['language'] is not None and validator.languageCode(args['language']):
-            param['language'] = args['language'].lower()
+    def _validArguments(self) -> None:
+        validFuncs = {
+            'id': validator.menuID,
+            'alias': validator.menuAlias,
+            'active': validator.active,
+            'order': validator.orderDirection,
+            'language': validator.languageCode
+        }
+        forDeleting = []
+        for key, value in self._args.items():
+            if value is None:
+                forDeleting.append(key)
+                continue
+            if key in validFuncs:
+                if not validFuncs[key](value):
+                    abort(400, message=f'Wrong {key}')
+                continue
+            else:
+                forDeleting.append(key)
+        for key in forDeleting:
+            del self._args[key]
 
+    def get(self):
+        self._setArguments()
+        self._validArguments()
+        params = self._getParams('active', 'order', 'language')
         repo = MenuRepo()
-        if args['parent'] is not None and validator.menuID(args['parent']):
-            data = repo.getMenuByID(args['parent'], **param)
-        elif args['alias'] is not None and validator.menuID(args['alias']):
-            data = repo.getMenuByAlias(args['alias'], **param)
-        else:
+        getDataMethods = {
+            'id': repo.getMenuByID,
+            'alias': repo.getMenuByAlias
+        }
+        for arg in getDataMethods:
+            if self._argsContains(arg):
+                data = getDataMethods[arg](self._getArg(arg), **params)
+                break
             abort(400, message='Menu ID or alias are required')
 
-        result = self._getMenuStructure()
+        result = self._getMenuStructure(data)
 
         return jsonify(result)
 
