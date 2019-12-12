5910290ef77f855b1fded0bf3aaa38bbf7e34fd7
Vadym Hutei <hutei@live.com>
2019-12-12 21:29:43 +0200

    [MENU REPO]

--- app/repositories/menu.py
+++ app/repositories/menu.py
@@ -3,8 +3,8 @@ from core import Repository
 
 class MenuRepo(Repository):
 
-    def getMenuByID(self, id_) -> list:
+    def getMenuByID(self, id_, **params) -> list:
         return []
 
-    def getMenuByAlias(self, alias) -> list:
+    def getMenuByAlias(self, alias, **params) -> list:
         return []
