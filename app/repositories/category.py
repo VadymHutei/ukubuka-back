b8aa9cebd47325c7631f14216c775861c38f1951
Vadym Hutei <hutei@live.com>
2020-02-11 09:55:33 +0200

    [CATEGORY] getCategories

--- app/repositories/category.py
+++ app/repositories/category.py
@@ -1,36 +1,34 @@
 from core import Repository
+from core.sql_query_builder import SelectQuery
 
 
 class CategoryRepo(Repository):
 
     def getCategories(self, params):
+        query = SelectQuery()
+        query.fields(
+            'c.`id`',
+            'c.`parent`',
+            'c.`order`',
+            'c.`added`',
+            'c.`is_active`',
+            'ct.`name`'
+        ),
+        query.table(('`categories`', 'c'))
+        language = params.get('language')
+        if language:
+            query.left_join(
+                ('`categories_text`', 'ct'),
+                'ct.`category_id` = c.`id`'
+            )
+            query.where('ct.`language` = %s')
+        query_string = query.render()
         connection = self._getConnection()
         try:
             with connection.cursor() as cursor:
-                categories_query = """
-                    SELECT
-                        `id`,
-                        `parent`,
-                        `order`,
-                        `added`,
-                        `is_active`
-                    FROM
-                        `categoriesd`
-                """
-                categories_text_query = """
-                    SELECT
-                        `language`,
-                        `name`
-                    FROM
-                        `categories_text`
-                    WHERE
-                        `category_id` IN (%s)
-                """
-                cursor.execute(categories_query)
+                cursor.execute(query_string, params['language'])
                 categories_result = cursor.fetchall()
-                print(categories_result)
-                cursor.execute(categories_text_query, (params['language'],))
-                items_result = cursor.fetchall()
+                return categories_result
         finally:
             connection.close()
 
