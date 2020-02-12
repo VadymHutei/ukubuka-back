c228c7952c9efb1740c0718a15d2035459c86675
Vadym Hutei <hutei@live.com>
2020-02-12 09:42:48 +0200

    [RESOURCE]

--- app/core/resource.py
+++ app/core/resource.py
@@ -8,6 +8,8 @@ class Resource(Rsrc):
             trim=True,
             bundle_errors=False
         )
+        self._args = {}
+        self._transform_methods = {}
         self._validation_methods = {}
 
     def _getParams(self, *args):
@@ -19,11 +21,18 @@ class Resource(Rsrc):
     def _getArg(self, arg):
         return self._args.get(arg, None)
 
+    def _parseArguments(self):
+        self._args = self._parser.parse_args()
+
+    def _transformArguments(self):
+        for arg, method in self._transform_methods.items():
+            if arg in self._args:
+                self._args[arg] = method(self._args[arg])
+
     def _validateArguments(self):
         forDeleting = []
         for key, value in self._args.items():
             if value is None:
-                forDeleting.append(key)
                 continue
             if key not in self._validation_methods:
                 forDeleting.append(key)
