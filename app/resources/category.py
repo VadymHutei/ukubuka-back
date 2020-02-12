b6eb5a3a5bc5f5eb31bb99af58834f38c2e8427e
Vadym Hutei <hutei@live.com>
2020-02-12 09:50:39 +0200

    [RESOURCES] Category

--- app/resources/category.py
+++ app/resources/category.py
@@ -1,7 +1,10 @@
 from flask import jsonify
 
 from core import Resource
-from core.validator import common as v_common, menu as v_menu
+from core.validator import (
+    common as v_common,
+    category as v_category
+)
 from repositories import CategoryRepo
 
 
@@ -21,17 +24,25 @@ class Category(Resource):
             case_sensitive=False,
             location='args'
         )
+        self._parser.add_argument(
+            'parent',
+            location='args'
+        )
+        self._transform_methods = {
+            'parent': lambda x: None if x == 'null' else x
+        }
         self._validation_methods = {
             'language': v_common.languageCode,
-            'is_active': v_common.active
+            'is_active': v_common.active,
+            'parent': v_category.categoryID
         }
 
     def get(self):
-        self._args = self._parser.parse_args()
+        self._parseArguments()
+        self._transformArguments()
         self._validateArguments()
-        params = self._getParams('language')
         repo = CategoryRepo()
-        result = repo.getCategories(params)
+        result = repo.getCategories(self._getArguments())
         return jsonify(result)
 
     def post(self):
@@ -45,29 +56,3 @@ class Category(Resource):
 
     def delete(self):
         pass
-
-    def wdeget(self):
-        self._setArguments()
-        validFuncs = {
-            'id': v_menu.menuID,
-            'alias': v_menu.menuAlias,
-            'active': v_common.active,
-            'order': v_common.orderDirection,
-            'language': v_common.languageCode
-        }
-        self._validArguments(validFuncs)
-        params = self._getParams('active', 'order', 'language')
-        repo = MenuRepo()
-        getDataMethods = {
-            'id': repo.getMenuByID,
-            'alias': repo.getMenuByAlias
-        }
-        for arg in getDataMethods:
-            if self._hasArg(arg):
-                try:
-                    data = getDataMethods[arg](self._getArg(arg), **params)
-                    data['items'] = self._getMenuStructure(data['items'])
-                    return jsonify(data)
-                except Exception:
-                    abort(500)
-        abort(400, message='Menu ID or alias are required')
