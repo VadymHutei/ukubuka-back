59918473bf2910acaf566275e524a1be0e9eafc2
Vadym Hutei <hutei@live.com>
2020-02-12 08:48:06 +0200

    [RESOURCE]

--- app/core/resource.py
+++ app/core/resource.py
@@ -1,7 +1,14 @@
-from flask_restful import Resource as Src
+from flask_restful import Resource as Rsrc, reqparse, abort
 
 
-class Resource(Src):
+class Resource(Rsrc):
+
+    def __init__(self):
+        self._parser = reqparse.RequestParser(
+            trim=True,
+            bundle_errors=False
+        )
+        self._validation_methods = {}
 
     def _getParams(self, *args):
         return {p: self._args[p] for p in args if p in self._args}
@@ -12,17 +19,16 @@ class Resource(Src):
     def _getArg(self, arg):
         return self._args.get(arg, None)
 
-    def _validArguments(self, validFuncs):
+    def _validateArguments(self):
         forDeleting = []
         for key, value in self._args.items():
             if value is None:
                 forDeleting.append(key)
                 continue
-            if key in validFuncs:
-                if not validFuncs[key](value):
-                    abort(400, message=f'Wrong {key}')
-                continue
-            else:
+            if key not in self._validation_methods:
                 forDeleting.append(key)
+                continue
+            if not self._validation_methods[key](value):
+                abort(400, message=f'Wrong {key}')
         for key in forDeleting:
             del self._args[key]
