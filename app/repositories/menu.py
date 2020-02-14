8410a9cebfa01e96349bdc774b7affbafee00e46
Vadym Hutei <hutei@live.com>
2020-02-14 09:00:50 +0200

    [MENU] Repo

--- app/repositories/menu.py
+++ app/repositories/menu.py
@@ -1,90 +1,44 @@
 from core import Repository
+from core.sql_query_builder import SelectQuery
 
 
 class MenuRepo(Repository):
 
-    def _separateMenuItems(self, id_, items) -> list:
-        menu_items = []
-        step_counter = -1
-        parents = [id_]
-        while step_counter != 0:
-            step_counter = 0
-            for item in items:
-                if item['parent'] in parents:
-                    menu_items.append(item)
-                    parents.append(item['id'])
-                    items.remove(item)
-                    step_counter += 1
-        return menu_items
+    def getMenus(self, params):
+        query = SelectQuery()
 
-    def getMenuByID(self, id_, **params) -> list:
+        query.fields(
+            'mi.id',
+            'mi.parent',
+            'mi.link',
+            'mi.order',
+            'mi.added',
+            'mi.is_active',
+            'mit.name',
+        )
+
+        query.table(('menu_items', 'mi'))
+
+        if 'is_active' in params:
+            query.where(('mi.is_active', '=', params['is_active']))
+
+        if 'language' in params:
+            query.leftJoin(
+                ('menu_items_text', 'mit'),
+                ('mit.item_id', '=', 'mi.id')
+            )
+            query.where(('mit.language', '=', params['language']))
+
+        if 'order' in params:
+            query.order(params['order'])
+
+        query_string = query.render()
+        print(query_string)
         connection = self._getConnection()
         try:
             with connection.cursor() as cursor:
-                menu_query = """
-                    SELECT
-                        `id`,
-                        `item_id`,
-                        `alias`
-                    FROM `menus`
-                    WHERE `id` = %s
-                """
-                items_query = """
-                    SELECT
-                        mi.`id`,
-                        mi.`parent`,
-                        mi.`link`,
-                        mi.`order`,
-                        mi.`added`,
-                        mi.`is_active`,
-                        mit.`name`
-                    FROM `menu_items` mi
-                    LEFT JOIN `menu_items_text` mit
-                        ON mit.`item_id` = mi.`id`
-                        AND mit.`language` = %s
-                """
-                cursor.execute(menu_query, (id_,))
-                menu_result = cursor.fetchone()
-                cursor.execute(items_query, (params['language'],))
-                items_result = cursor.fetchall()
+                cursor.execute(query_string)
+                categories_result = cursor.fetchall()
+                return categories_result
         finally:
             connection.close()
-        args = [menu_result['item_id'], items_result]
-        menu_result['items'] = self._separateMenuItems(*args)
-        return menu_result
-
-    def getMenuByAlias(self, alias, **params) -> list:
-        connection = self._getConnection()
-        try:
-            with connection.cursor() as cursor:
-                menu_query = """
-                    SELECT
-                        `id`,
-                        `item_id`,
-                        `alias`
-                    FROM `menus`
-                    WHERE `alias` = %s
-                """
-                items_query = """
-                    SELECT
-                        mi.`id`,
-                        mi.`parent`,
-                        mi.`link`,
-                        mi.`order`,
-                        mi.`added`,
-                        mi.`is_active`,
-                        mit.`name`
-                    FROM `menu_items` mi
-                    LEFT JOIN `menu_items_text` mit
-                        ON mit.`item_id` = mi.`id`
-                        AND mit.`language` = %s
-                """
-                cursor.execute(menu_query, (alias,))
-                menu_result = cursor.fetchone()
-                cursor.execute(items_query, (params['language'],))
-                items_result = cursor.fetchall()
-        finally:
-            connection.close()
-        args = [menu_result['item_id'], items_result]
-        menu_result['items'] = self._separateMenuItems(*args)
-        return menu_result
