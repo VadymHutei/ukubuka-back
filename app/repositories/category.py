3545ec8dc65af44a7fe85c3a1c7f77e4c5892f26
Vadym Hutei <hutei@live.com>
2020-02-14 09:00:08 +0200

    [CATEGORY] Repo

--- app/repositories/category.py
+++ app/repositories/category.py
@@ -6,31 +6,33 @@ class CategoryRepo(Repository):
 
     def getCategories(self, params):
         query = SelectQuery()
+
         query.fields(
-            'c.`id`',
-            'c.`parent`',
-            'c.`order`',
-            'c.`added`',
-            'c.`is_active`',
-            'ct.`name`'
-        ),
-        query.table(('`categories`', 'c'))
+            'c.id',
+            'c.parent',
+            'c.order',
+            'c.added',
+            'c.is_active',
+            'ct.name'
+        )
+
+        query.table(('categories', 'c'))
 
         if 'is_active' in params:
-            query.where(f'c.`is_active` = "{params["is_active"]}"')
+            query.where(('c.is_active', '=', params['is_active']))
 
         if 'parent' in params:
             if params['parent'] is None:
-                query.where('c.`parent` IS NULL')
+                query.where(('c.parent', 'IS NULL'))
             else:
-                query.where(f'c.`parent` = {params["parent"]}')
+                query.where(('c.parent', '=', params['parent']))
 
         if 'language' in params:
             query.leftJoin(
-                ('`categories_text`', 'ct'),
-                'ct.`category_id` = c.`id`'
+                ('categories_text', 'ct'),
+                ('ct.category_id', '=', 'c.id')
             )
-            query.where(f'ct.`language` = "{params["language"]}"')
+            query.where(('ct.language', '=', params['language']))
 
         query_string = query.render()
         connection = self._getConnection()
