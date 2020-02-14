d1a52ef955b30404ae0291309441b621ff6e06db
Vadym Hutei <hutei@live.com>
2020-02-14 09:00:41 +0200

    [MENU] Rosource

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,7 +1,8 @@
-from resources.language import Language
-from resources.menu import Menu
-from resources.menu_item import MenuItem
 from resources.category import Category
+from resources.menu import Menu
+
+from resources.language import Language
+from resources.menu_item import MenuItem
 from resources.product import Product
 from resources.sku import SKU
 from resources.characteristic import Characteristic
--- app/resources/menu.py
+++ app/resources/menu.py
@@ -1,46 +1,72 @@
 from flask import jsonify
-from flask_restful import reqparse, abort
 
-from core import Resource, validator
+from core import Resource
+from core.validator import (
+    common as v_common,
+    menu as v_menu
+)
 from repositories import MenuRepo
 
 
 class Menu(Resource):
 
-    def _setArguments(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('id', type=int, location='args')
-        parser.add_argument('alias', location='args')
-        parser.add_argument('active', location='args')
-        parser.add_argument('order', location='args')
-        parser.add_argument('language', location='headers')
-        self._args = parser.parse_args()
+    def __init__(self):
+        super().__init__()
+        self._parser.add_argument(
+            'language',
+            required=True,
+            location='headers'
+        )
+        self._parser.add_argument(
+            'active',
+            dest='is_active',
+            choices=('y', 'n'),
+            case_sensitive=False,
+            store_missing=False,
+            location='args'
+        )
+        self._parser.add_argument(
+            'order',
+            choices=('asc', 'desc'),
+            case_sensitive=False,
+            store_missing=False,
+            location='args'
+        )
+        self._parser.add_argument(
+            'order_by',
+            action='append',
+            store_missing=False,
+            location='args'
+        )
+        self._transform_methods = {
+            'order_by': lambda x: [tuple(y.split('-')[-2:]) for y in x]
+        }
+        self._validation_methods = {
+            'language': v_common.languageCode,
+            'is_active': v_common.active,
+            'order': v_common.orderDirection
+        }
 
     def get(self):
-        self._setArguments()
-        validFuncs = {
-            'id': validator.menu.menuID,
-            'alias': validator.menu.menuAlias,
-            'active': validator.common.active,
-            'order': validator.common.orderDirection,
-            'language': validator.common.languageCode
-        }
-        self._validArguments(validFuncs)
-        params = self._getParams('active', 'order', 'language')
+        self._parseArguments()
+        self._transformArguments()
+        self._validateArguments()
         repo = MenuRepo()
-        getDataMethods = {
-            'id': repo.getMenuByID,
-            'alias': repo.getMenuByAlias
-        }
-        for arg in getDataMethods:
-            if self._hasArg(arg):
-                try:
-                    data = getDataMethods[arg](self._getArg(arg), **params)
-                    data['items'] = self._getMenuStructure(data['items'])
-                    return jsonify(data)
-                except Exception:
-                    abort(500)
-        abort(400, message='Menu ID or alias are required')
+        result = repo.getMenus(self._getArguments())
+        return jsonify(result)
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
+
 
     def _getMenuStructure(self, items) -> dict:
         result = []
