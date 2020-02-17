cab9b8afa234ebaba31f2613c25bcc2d183d04b3
Vadym Hutei <hutei@live.com>
2020-02-17 09:21:57 +0200

    [main.py]

--- app/main.py
+++ app/main.py
@@ -2,28 +2,18 @@ from flask import Flask
 from flask_restful import Api
 
 from resources import (
-    Language,
-    Menu,
-    MenuItem,
     Category,
     Product,
-    SKU,
-    Characteristic,
-    Currency
+    Menu
 )
 
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Language, '/languages')
-api.add_resource(Menu, '/menus')
-api.add_resource(MenuItem, '/menus/items')
 api.add_resource(Category, '/categories')
 api.add_resource(Product, '/products')
-api.add_resource(SKU, '/sku')
-api.add_resource(Characteristic, '/characteristics')
-api.add_resource(Currency, '/currencies')
+api.add_resource(Menu, '/menus')
 
 
 @app.route('/', methods=['GET'])
