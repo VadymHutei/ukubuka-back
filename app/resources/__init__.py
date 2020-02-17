0d47d4f0752fde3028ee0079e21d848bc31460ab
Vadym Hutei <hutei@live.com>
2020-02-17 09:20:09 +0200

    [PRODUCT] Resource

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,9 +1,9 @@
 from resources.category import Category
+from resources.product import Product
 from resources.menu import Menu
 
 from resources.language import Language
 from resources.menu_item import MenuItem
-from resources.product import Product
 from resources.sku import SKU
 from resources.characteristic import Characteristic
 from resources.currency import Currency
--- app/resources/product.py
+++ app/resources/product.py
@@ -1,10 +1,62 @@
 from flask import jsonify
-from flask_restful import Resource
+
+from core import Resource
+from core.validator import (
+    common as v_common,
+    category as v_category
+)
+from repositories import ProductRepo
 
 
 class Product(Resource):
 
-    def get(self):
-        return jsonify(
-            {}
+    def __init__(self):
+        super().__init__()
+        self._parser.add_argument(
+            'language',
+            required=True,
+            location='headers'
         )
+        self._parser.add_argument(
+            'active',
+            dest='is_active',
+            choices=('y', 'n'),
+            case_sensitive=False,
+            store_missing=False,
+            location='args'
+        )
+        self._parser.add_argument(
+            'category',
+            dest='category_id',
+            action='append',
+            store_missing=False,
+            location='args'
+        )
+        self._transform_methods = {
+            'category_id': lambda x: int(x)
+        }
+        self._validation_methods = {
+            'language': v_common.languageCode,
+            'is_active': v_common.active,
+            'category_id': v_category.categoryID
+        }
+
+    def get(self):
+        self._parseArguments()
+        self._transformArguments()
+        self._validateArguments()
+        repo = ProductRepo()
+        result = repo.getProducts(self._getArguments())
+        return jsonify(result)
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def patch(self):
+        pass
+
+    def delete(self):
+        pass
